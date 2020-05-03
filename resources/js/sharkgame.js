ec6ebc4c8757106422e4604d279f94d21ef32f07
Patrick Gietl <e1527148@student.tuwien.ac.at>
2020-05-03 23:56:53 +0200

    - sharkgame canvas draw update

--- resources/js/sharkgame.js
+++ resources/js/sharkgame.js
@@ -1,6 +1,26 @@
-window.onload = function() {
+$('document').ready(function(){
   var canvas = document.getElementById("game");
   var ctx = canvas.getContext("2d");
-  var img = document.getElementById("shark");
-  ctx.drawImage(img, 10, 10);
-};
+  var x = canvas.width/2;
+  var y = canvas.height-30;
+  var dx = 2;
+  var dy = -2;
+  var drawing = document.getElementById("shark");
+  //drawing = new Image();
+  //drawing.onload = function() {
+  //  setInterval(draw, 10);
+  //};
+  //drawing.src = "../../resources/binary/sharks/great_shark.PNG"; // can also be a remote URL e.g. http://
+
+  function drawShark() {
+    ctx.drawImage(drawing,canvas.width/2, canvas.height/2);
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawShark();
+    x += dx;
+    y += dy;
+  }
+  setInterval(draw, 10);
+});
