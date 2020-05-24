e4ed41b9bd792eae22e9b55c59cd3d576c47ead9
Patrick Gietl <e1527148@student.tuwien.ac.at>
2020-05-25 00:30:54 +0200

    - drag and drop korrekturen

--- resources/js/basics-drag-and-drop.js
+++ resources/js/basics-drag-and-drop.js
@@ -22,36 +22,38 @@ function onDragEnter(event) {
 
 function onDrop(event) {
   const target = event.target;
-  if (target && dragged && target.children[0].innerHTML === dragged.id) {
+  var targetName = target.children[0].innerHTML;
+  if (target && dragged && targetName === dragged.id) {
     event.preventDefault();
     // Get the id of the target and add the moved element to the target's DOM
     dragged.parentNode.removeChild(dragged);
     dragged.style.opacity = '';
     target.appendChild(dragged);
     dragged.style.draggable = false;
-	$('#infoModal').modal('show')
+	$('#infoModal').modal('show');
+
 	if(dragged.id=="Weißer Hai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/White_shark.jpg";
-	document.getElementById("text").textContent="Der weiße Hai kommt in fast allen Meeren der Am vor. Doch am häufigsten werden sie in Australien, im westlichen Nordatlantik, vor der Südküste Afrikas und im Mittelmeer beobachtet. Weiße Haie erbeuten alle möglichen Meerestiere, etwa Robben, kleine Wale, Thunfische oder Tintenfische. Wenn nötig, fressen sie aber auch Aas. Menschen stehen glücklicherweise nicht auf dem Speiseplan. Sie werden im Schnitt 4 bis 5 Meter lang und bis zu 2000 Kilogramm schwer. Forscher vermuten jedoch, dass sie etwa so alt wie Menschen werden können.";
+	document.getElementById("text").textContent="Der weiße Hai kommt in fast allen Meeren der Welt vor. Am häufigsten werden sie allerdings in Australien, im westlichen Nordatlantik, vor der Südküste Afrikas und im Mittelmeer beobachtet. Weiße Haie erbeuten alle möglichen Meerestiere, etwa Robben, kleine Wale, Thunfische oder Tintenfische. Menschen stehen glücklicherweise nicht auf dem Speiseplan. Sie werden im Schnitt 4 bis 5 Meter lang und bis zu 2000 Kilogramm schwer. Forscher vermuten, dass sie etwa so alt wie Menschen werden können.";
 
 	}
 	if(dragged.id=="Tigerhai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Tiger_shark.jpg";
-	document.getElementById("text").textContent="Der Tigerhai kann je nachdem ob er weiblich oder männlich ist 325 bis 550 Zentimeter lang werden oder 900 Kilogramm wiegen. Der Tigerhai kann über 7 m und ein Gewicht von über 800 kg erreichen. Außerdem wird er maximal ungefähr 750 cm lang aber seine durchschnittliche Länge beträgt ca. 500 cm. Sie können bis zu 50 Jahre alt werden. Der Tigerhai frisst eigentlich fast alles: andere Haifische, Knochenfische, Seevögel, Meeressäuger, Meeresschildkröten, Wirbellose (also Krebse und Insekten).zur Nahrungsvielfältigkeit des Tigerhais gehört neben den Fischen und den Meeresschildkröten auch Meeressäuger wie kleine Wale und  Delfine  sowie andere Haie.";
+	document.getElementById("text").textContent="Der Tigerhai kann über 7 m und ein Gewicht von über 800 kg erreichen, und bis zu 50 Jahre alt werden. Der Tigerhai frisst eigentlich fast alles: andere Haifische, Knochenfische, Seevögel, Meeressäuger, Meeresschildkröten, Wirbellose (also Krebse und Insekten), usw. Zur Nahrungsvielfältigkeit des Tigerhais gehört neben den Fischen und den Meeresschildkröten auch Meeressäuger wie kleine Wale, Delfine und sogar andere Haie.";
 
 	}
 	if(dragged.id=="Sägehai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Sawfish.jpg";
-	document.getElementById("text").textContent="Sägehaie sind für ihre lange und schmale Schnauze, die aus kleinen Zähnen bestehen bekannt. Sie fressen kleine Fische, Krebstiere aber auch Tintenfischarten. Die größte Art bildet der Sechskiemer-Sägehai mit etwa 1,5 Metern. Was du noch wissen solltest ist das Sägehaie im wesentlichen Atlantik leben.";
+	document.getElementById("text").textContent="Sägehaie sind für ihre lange und schmale Schnauze, die aus kleinen Zähnen bestehen bekannt. Sie fressen kleine Fische, Krebstiere aber auch Tintenfischarten. Die größte Art bildet der Sechskiemer-Sägehai mit etwa 1,5 Metern. Sägehaie findet man hauptsächlich im Atlantik.";
 
 	}
 	if(dragged.id=="Walhai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Whale_Shark.jpg";
-	document.getElementById("text").textContent="Der größte Hai ist der Walhai. Er hat eine eher bläuliche oder gräuliche Farbe. Außerdem lebt der Walhai in den tropischen oder subtropischen Regionen auf dem offenen Meer. Die Forscher sagen das Walhai bis zu 20 Meter lang und ein Gewichtvon 35 Tonnen erreichen können. Noch dazu werden sie geschätzt 100 Jahre alt.Die dickste Haut, nämlich 10 - 15 cm. Aber normalerweise wird er nur bis zu 15Tonnen schwer und 14 - 15 Meter groß. Der Walhai ernäht sich von Plankton,Quallen, Sardinen und andere Kleinlebewesen.";
+	document.getElementById("text").textContent="Der größte Hai ist der Walhai. Er hat eine eher bläuliche oder gräuliche Farbe. Außerdem lebt der Walhai in den tropischen oder subtropischen Regionen auf dem offenen Meer. Forscher meinen, dass der Walhai bis zu 20 Meter lang und ein Gewicht von bis zu 35 Tonnen erreichen kann. Im Schnitt wird er jedoch nur bis zu 15 Tonnen schwer und 14 - 15 Meter groß. Walhaie werden mit bis zu 100 Jahre, im Vergleich zu vielen anderen Haiarten, sehr alt. Er besitzt, mit bis zu 15cm Dicke, eine sehr robuste Haut. Der Walhai ernäht sich von als sogenannter Filtrierer, von Plankton, Quallen, Sardinen und andere Kleinlebewesen, indem er das Meerwasser nach diesen filtert";
 	}
 	if(dragged.id=="Hammerhai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Hammerhead_Shark.jpg";
-	document.getElementById("text").textContent="Wurde nach seiner besonderen Kopfform benannt. Er lebt ungefähr 20-25 Jahre under kann höchstens 5,5 bis 6,1 Meter lang werden. Er frisst jede Menge wirbellose Tieren wie Krebse und Kalmaren und andere kleine Fische.";
+	document.getElementById("text").textContent="Wurde nach seiner besonderen Kopfform benannt. Er lebt ungefähr 20-25 Jahre und kann höchstens 5,5 bis 6,1 Meter lang werden. Er frisst wirbellose Tiere wie Krebse und Kalmaren, sowie andere kleine Fische.";
 
 	}
 
@@ -67,31 +69,31 @@ function onDrop(event) {
 	else{
 	$('#infoModal').modal('show')
 
-	document.getElementById("infoModalLongTitle").innerHTML = "Flasch! der "+dragged.id;
+	document.getElementById("infoModalLongTitle").innerHTML = "Falsch! der " + targetName;
 	document.getElementById("infoModalLongTitle").style.color = '#ff0000';
 
-	if(dragged.id=="Weißer Hai"){
+	if(targetName=="Weißer Hai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/White_shark.jpg";
-	document.getElementById("text").textContent="Der weiße Hai kommt in fast allen Meeren der Am vor. Doch am häufigsten werden sie in Australien, im westlichen Nordatlantik, vor der Südküste Afrikas und im Mittelmeer beobachtet. Weiße Haie erbeuten alle möglichen Meerestiere, etwa Robben, kleine Wale, Thunfische oder Tintenfische. Wenn nötig, fressen sie aber auch Aas. Menschen stehen glücklicherweise nicht auf dem Speiseplan. Sie werden im Schnitt 4 bis 5 Meter lang und bis zu 2000 Kilogramm schwer. Forscher vermuten jedoch, dass sie etwa so alt wie Menschen werden können.";
+	document.getElementById("text").textContent="Der weiße Hai kommt in fast allen Meeren der Welt vor. Am häufigsten werden sie allerdings in Australien, im westlichen Nordatlantik, vor der Südküste Afrikas und im Mittelmeer beobachtet. Weiße Haie erbeuten alle möglichen Meerestiere, etwa Robben, kleine Wale, Thunfische oder Tintenfische. Menschen stehen glücklicherweise nicht auf dem Speiseplan. Sie werden im Schnitt 4 bis 5 Meter lang und bis zu 2000 Kilogramm schwer. Forscher vermuten, dass sie etwa so alt wie Menschen werden können.";
 
 	}
-	if(dragged.id=="Tigerhai"){
+	if(targetName=="Tigerhai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Tiger_shark.jpg";
-	document.getElementById("text").innerHTML="Der Tigerhai kann je nachdem ob er weiblich oder männlich ist 325 bis 550 Zentimeter lang werden oder 900 Kilogramm wiegen. Der Tigerhai kann über 7 m und ein Gewicht von über 800 kg erreichen. Außerdem wird er maximal ungefähr 750 cm lang aber seine durchschnittliche Länge beträgt ca. 500 cm. Sie können bis zu 50 Jahre alt werden. Der Tigerhai frisst eigentlich fast alles: andere Haifische, Knochenfische, Seevögel, Meeressäuger, Meeresschildkröten, Wirbellose (also Krebse und Insekten).zur Nahrungsvielfältigkeit des Tigerhais gehört neben den Fischen und den Meeresschildkröten auch Meeressäuger wie kleine Wale und  Delfine  sowie andere Haie.";
+	document.getElementById("text").textContent="Der Tigerhai kann über 7 m und ein Gewicht von über 800 kg erreichen, und bis zu 50 Jahre alt werden. Der Tigerhai frisst eigentlich fast alles: andere Haifische, Knochenfische, Seevögel, Meeressäuger, Meeresschildkröten, Wirbellose (also Krebse und Insekten), usw. Zur Nahrungsvielfältigkeit des Tigerhais gehört neben den Fischen und den Meeresschildkröten auch Meeressäuger wie kleine Wale, Delfine und sogar andere Haie.";
 
 	}
-	if(dragged.id=="Sägehai"){
+	if(targetName=="Sägehai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Sawfish.jpg";
-	document.getElementById("text").textContent="Sägehaie sind für ihre lange und schmale Schnauze, die aus kleinen Zähnen bestehen bekannt. Sie fressen kleine Fische, Krebstiere aber auch Tintenfischarten. Die größte Art bildet der Sechskiemer-Sägehai mit etwa 1,5 Metern. Was du noch wissen solltest ist das Sägehaie im wesentlichen Atlantik leben.";
+	document.getElementById("text").textContent="Sägehaie sind für ihre lange und schmale Schnauze, die aus kleinen Zähnen bestehen bekannt. Sie fressen kleine Fische, Krebstiere aber auch Tintenfischarten. Die größte Art bildet der Sechskiemer-Sägehai mit etwa 1,5 Metern. Sägehaie findet man hauptsächlich im Atlantik.";
 
 	}
-	if(dragged.id=="Walhai"){
+	if(targetName=="Walhai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Whale_Shark.jpg";
-	document.getElementById("text").textContent="Der größte Hai ist der Walhai. Er hat eine eher bläuliche oder gräuliche Farbe. Außerdem lebt der Walhai in den tropischen oder subtropischen Regionen auf dem offenen Meer. Die Forscher sagen das Walhai bis zu 20 Meter lang und ein Gewichtvon 35 Tonnen erreichen können. Noch dazu werden sie geschätzt 100 Jahre alt.Die dickste Haut, nämlich 10 - 15 cm. Aber normalerweise wird er nur bis zu 15Tonnen schwer und 14 - 15 Meter groß. Der Walhai ernäht sich von Plankton,Quallen, Sardinen und andere Kleinlebewesen.";
+	document.getElementById("text").textContent="Der größte Hai ist der Walhai. Er hat eine eher bläuliche oder gräuliche Farbe. Außerdem lebt der Walhai in den tropischen oder subtropischen Regionen auf dem offenen Meer. Forscher meinen, dass der Walhai bis zu 20 Meter lang und ein Gewicht von bis zu 35 Tonnen erreichen kann. Im Schnitt wird er jedoch nur bis zu 15 Tonnen schwer und 14 - 15 Meter groß. Walhaie werden mit bis zu 100 Jahre, im Vergleich zu vielen anderen Haiarten, sehr alt. Er besitzt, mit bis zu 15cm Dicke, eine sehr robuste Haut. Der Walhai ernäht sich von als sogenannter Filtrierer, von Plankton, Quallen, Sardinen und andere Kleinlebewesen, indem er das Meerwasser nach diesen filtert";
 	}
-	if(dragged.id=="Hammerhai"){
+	if(targetName=="Hammerhai"){
 	document.getElementById("sharkimage").src="../../resources/binary/img/Hammerhead_Shark.jpg";
-	document.getElementById("text").textContent="Wurde nach seiner besonderen Kopfform benannt. Er lebt ungefähr 20-25 Jahre under kann höchstens 5,5 bis 6,1 Meter lang werden. Er frisst jede Menge wirbellose Tieren wie Krebse und Kalmaren und andere kleine Fische.";
+	document.getElementById("text").textContent="Wurde nach seiner besonderen Kopfform benannt. Er lebt ungefähr 20-25 Jahre und kann höchstens 5,5 bis 6,1 Meter lang werden. Er frisst wirbellose Tiere wie Krebse und Kalmaren, sowie andere kleine Fische.";
 
 	}
 	
